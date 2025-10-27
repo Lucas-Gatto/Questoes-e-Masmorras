2807debb69001b49a2afa4afd39735ecb2f57406
Milene <mimilene2018@gmail.com>
2025-10-27 01:20:54 -0300

    Fix: Acessibilidade de cor
    Mudanças nas cores para que se adequem ao nível AA e AAA da WCAG

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./global_style.css";
 
 // Layouts
-import LayoutDeslogado from './components/layouts/LayoutDeslogado.jsx';
-import LayoutLogado from './components/layouts/LayoutLogado.jsx';
-import LayoutAluno from './components/layouts/LayoutAluno.jsx'; // Layout do Aluno
+import LayoutDeslogado from "./components/layouts/LayoutDeslogado.jsx";
+import LayoutLogado from "./components/layouts/LayoutLogado.jsx";
+import LayoutAluno from "./components/layouts/LayoutAluno.jsx"; // Layout do Aluno
 
 // Páginas
-import Home from './pages/home.jsx';
-import Login from './pages/login.jsx';
-import SuasAventuras from './pages/suas-aventuras.jsx';
-import NovaAventura from './pages/nova-aventura.jsx';
-import EditarAventura from './pages/editar-aventura.jsx';
-import EditarSala from './pages/editar-sala.jsx';
-import IniciarAventura from './pages/iniciar-aventura.jsx'; // <-- Importação necessária
-import SalaDeJogo from './pages/sala-de-jogo.jsx';
-import EntrarAventura from './pages/entrar-aventura.jsx';
-import EscolherClasse from './pages/escolher-classe.jsx';
-import SalasAluno from './pages/salas-aluno.jsx';
+import Home from "./pages/home.jsx";
+import Login from "./pages/login.jsx";
+import SuasAventuras from "./pages/suas-aventuras.jsx";
+import NovaAventura from "./pages/nova-aventura.jsx";
+import EditarAventura from "./pages/editar-aventura.jsx";
+import EditarSala from "./pages/editar-sala.jsx";
+import IniciarAventura from "./pages/iniciar-aventura.jsx"; // <-- Importação necessária
+import SalaDeJogo from "./pages/sala-de-jogo.jsx";
+import EntrarAventura from "./pages/entrar-aventura.jsx";
+import EscolherClasse from "./pages/escolher-classe.jsx";
+import SalasAluno from "./pages/salas-aluno.jsx";
 
 function App() {
   return (
@@ -27,31 +28,36 @@ function App() {
         <Routes>
           {/* --- GRUPO 1: Rotas Públicas (com LayoutDeslogado) --- */}
           <Route element={<LayoutDeslogado />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
           </Route>
 
           {/* --- GRUPO 2: Rotas de Gerenciamento (com LayoutLogado) --- */}
           <Route element={<LayoutLogado />}>
-            <Route path='/suas-aventuras' element={<SuasAventuras />} />
-            <Route path='/nova-aventura' element={<NovaAventura />} />
-            <Route path='/editar-aventura/:id' element={<EditarAventura />} />
-            <Route path="/aventura/:aventuraId/sala/:salaId/editar" element={<EditarSala />} />
+            <Route path="/suas-aventuras" element={<SuasAventuras />} />
+            <Route path="/nova-aventura" element={<NovaAventura />} />
+            <Route path="/editar-aventura/:id" element={<EditarAventura />} />
+            <Route
+              path="/aventura/:aventuraId/sala/:salaId/editar"
+              element={<EditarSala />}
+            />
           </Route>
 
           {/* --- GRUPO 3: Rotas de Jogo Ativo (Mestre - layout próprio) --- */}
-           {/* 👇 ROTA IMPORTANTE AQUI (FORA do LayoutLogado) 👇 */}
-          <Route path="/iniciar-aventura/:aventuraId" element={<IniciarAventura />} />
+          {/* 👇 ROTA IMPORTANTE AQUI (FORA do LayoutLogado) 👇 */}
+          <Route
+            path="/iniciar-aventura/:aventuraId"
+            element={<IniciarAventura />}
+          />
           <Route path="/aventura/:aventuraId/jogar" element={<SalaDeJogo />} />
 
           {/* --- GRUPO 4: Rotas do Aluno (com LayoutAluno) --- */}
           <Route element={<LayoutAluno />}>
-            <Route path='/entrar-aventura' element={<EntrarAventura />} />
-            <Route path='/escolher-classe' element={<EscolherClasse />} />
-            <Route path='/salas-aluno' element={<SalasAluno />} />
-             {/* Adicione outras rotas do aluno aqui dentro se necessário */}
+            <Route path="/entrar-aventura" element={<EntrarAventura />} />
+            <Route path="/escolher-classe" element={<EscolherClasse />} />
+            <Route path="/salas-aluno" element={<SalasAluno />} />
+            {/* Adicione outras rotas do aluno aqui dentro se necessário */}
           </Route>
-
         </Routes>
       </BrowserRouter>
     </div>

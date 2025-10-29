a2bcc0510ee49e2b9dd44961372bfa6e41bf284b
Milene <mimilene2018@gmail.com>
2025-10-29 20:59:30 -0300

    fix: WAVE (entrar-aventura)
    Correção da tela Entrar-Aventura

--- frontend/src/pages/entrar-aventura.jsx
+++ frontend/src/pages/entrar-aventura.jsx
@@ -5,7 +5,8 @@ const EntrarAventura = () => {
   const [aventuraSelecionada, setAventuraSelecionada] = useState(null);
 
   useEffect(() => {
-    const aventurasSalvas = JSON.parse(localStorage.getItem("minhas_aventuras")) || [];
+    const aventurasSalvas =
+      JSON.parse(localStorage.getItem("minhas_aventuras")) || [];
 
     // Aqui você pode escolher a lógica de seleção:
     // Exemplo: pegar a última aventura criada
@@ -17,17 +18,19 @@ const EntrarAventura = () => {
 
   return (
     <div className="aventuras-page-container" role="main">
-
-
       {aventuraSelecionada && (
         <h1 className="titulo-aventura">{aventuraSelecionada.titulo}</h1>
       )}
 
-      <input
-        id="login-senha"
-        type="password"
-        placeholder="Digite o nome do seu personagem..."
-      />
+      <label className="nomePersonagem">
+        {" "}
+        Nome do Personagem
+        <input
+          id="login-senha"
+          type="password"
+          placeholder="Digite o nome do seu personagem..."
+        />
+      </label>
 
       <div className="btn-entrar-aventura-container">
         <button className="btn-entrar-aventura">Entrar na Aventura</button>

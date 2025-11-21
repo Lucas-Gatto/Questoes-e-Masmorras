447544bc66e61bf48667c0552e25b96af5f658d1
Ricardo Campos <106117682+littlerichard01@users.noreply.github.com>
2025-11-21 15:53:45 -0300

    Correção avatar em header de aventura

--- frontend/src/components/HeaderAventura.jsx
+++ frontend/src/components/HeaderAventura.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './header-aventura.css';
 import qmLogo from '../assets/q&mLogo.png';  // Reutilizando o logo
+import { AvatarContext } from '../contexts/AvatarContext';
 
 const HeaderAventura = () => {
   const navigate = useNavigate();
+  const { avatar } = useContext(AvatarContext);
 
   return (
     <header className="header-aventura-container">
       <div className="header-aventura-left">
-        <div className="avatar-placeholder"></div>
+        {/* Avatar do usuário (igual ao comportamento das outras telas) */}
+        <div className="avatar-placeholder">
+            <img src={avatar} alt="Avatar do usuário" className="avatar-mini" />
+          </div>
         
         <button 
           className="encerrar-aventura-btn" 
